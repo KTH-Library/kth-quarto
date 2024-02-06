36f57fe7848b36ad805e16d0b639e2f4bb0ccc1f
Markus Skyttner <mskyttner@pm.me>
2024-02-06 17:35:53 +0100

    add web slides template (reveal.js format)

---
+++ _extensions/kth-quarto/img/bg/optimg.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mogrify -thumbnail 800 -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace gray -matte -channel A +level 0,70% +channel *.png
+optipng *.png
